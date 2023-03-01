b3f121d3123d5dfe01b96a3cc252dc9830d94671
Matthew Halton <matthewhalton@hotmail.co.uk>
2023-03-01 12:09:19 +0000

    added add book

---
+++ src/books/controllers.js
@@ -0,0 +1,41 @@
+const Book = require("./model");
+
+const addBook = async (req, res) => {
+    try {
+        // const book = await Book.create({
+        //     title: req.body.title,
+        //     author: req.body.author,
+        //     genre: req.body.genre,
+        // });
+
+        const book = await Book.create(req.body);
+
+        console.log(book);
+
+        // const successResponse = {
+        //     message: "success",
+        //     newBook: book,
+        // };
+
+        // res.status(201).json(successResponse);
+
+        res.status(201).json({ message: "success", newBook: book });
+
+
+
+    } catch { error } {
+        console.log(error);
+        // const errorResponse = {
+        //     errorMessage: error.message,
+        //     error: error,
+        // };
+        // res.status(501).json(errorResponse);
+
+        res.status(501).json({ errorMessage: error.message, error:error });
+    }
+
+};
+
+module.exports = {
+    addBook,
+};
--- src/server.js
+++ src/server.js
@@ -1,16 +1,29 @@
 require("dotenv").config();
+console.log(process.env.MY_SUPER_SECRET);
 const express = require("express");
 
 const port = process.env.PORT || 5001;
+
+const Book = require("./books/model");
+
+const bookRouter = require("./books/routes");
 
 const app = express();
 
 app.use(express.json());
 
+const syncTables = () => {
+    Book.sync();
+};
+
+app.use(bookRouter);
+
 app.get("/health", (req, res) => 
 res.status(200).json({ message: "API is working" })
 );
 
+
 app.listen(port, () => {
+    syncTables();
     console.log(`Server is listening on port ${port}`);
 });
