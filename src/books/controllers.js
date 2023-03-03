9f9cbcb90bd11a8664f902513bbf026986516858
Matthew Halton <matthewhalton@hotmail.co.uk>
2023-03-03 09:55:59 +0000

    added authoration between databases

--- src/books/controllers.js
+++ src/books/controllers.js
@@ -23,7 +23,7 @@ const addBook = async (req, res) => {
 
 
 
-    } catch { error } {
+    } catch ( error ) {
         console.log(error);
         // const errorResponse = {
         //     errorMessage: error.message,
@@ -31,11 +31,102 @@ const addBook = async (req, res) => {
         // };
         // res.status(501).json(errorResponse);
 
-        res.status(501).json({ errorMessage: error.message, error:error });
+        res.status(501).json({ errorMessage: error.message, error: error });
+    }
+};
+
+
+const getAllbooks = async (req, res) => {
+    try {
+        const allBooks = await Book.findAll(req.body);
+
+        res.status(201).json({ message: "Success", getBook: allBooks });
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(501).json({ errorMessage: error.message, error: error });
     }
 
 };
 
+const getSingleBookbyTitle = async (req, res) => {
+    try {
+        const book = await Book.findOne({ where: { title: req.body.title }});
+        console.log(book)
+        res.status(200).json({ message: "success", book: book });
+    } catch (error) {
+        res.status(501).json({ errorMessage: error.message, error: error });
+    }
+};
+
+
+// {
+//     "title" : "book4"
+//     "newTitle" : "updated title"
+// }
+
+
+const updatebooks = async (req, res) => {
+    try {
+        const updatebook = await Book.update
+            ({ [req.body.updateKey]: req.body.updateValue }, {
+                where: {
+                    title: req.body.title
+                }});
+
+                res.status(501).json({ message: "Success", updatebook });
+            }
+
+        catch (error) { 
+        
+        res.status(201).json({ errorMessage: error.message, error: error });
+    };
+
+    console.log()
+}
+
+
+
+
+// Testingupdate
+
+// const updateBook = await Book.update (
+//     {genre: req.body. newGenre}, 
+//     { where: { title: req.body.title } }
+// );
+
+
+
+const deletebook = async (req, res) => {
+    try {
+        const result = await Book.destroy({ where: { title: req.body.title } });
+
+        res.status(201).json({ message: "success", result: result })
+    } catch (error) {
+        res.status(501).json({ errorMessage: error.message, error: error });
+
+    }
+};
+
+const deleteAllbooks = async (req, res) => {
+    try {
+        const result = await Book.destroy({ truncate: true })
+        res.status(200).json({ message: "success", result: result });
+    } catch (error) {
+        res.status(501).json({ errorMessage: error.message, error: error });
+    }
+};
+
+
+
 module.exports = {
     addBook,
+    getAllbooks,
+    updatebooks,
+    deletebook,
+    deleteAllbooks,
+    getSingleBookbyTitle,
+    
+
 };
--- src/books/routes.js
+++ src/books/routes.js
@@ -1,9 +1,17 @@
-const {Router} = require("express")
+const { Router } = require("express")
 
 const bookRouter = Router();
 
-const { addBook } = require ("./controllers")
+const { addBook, updatebooks, deletebook, deleteAllbooks } = require ("./controllers")
+const { getAllbooks, getSingleBookbyTitle } = require("./controllers")
 
-bookRouter.post("/health/addbook", addBook);
+bookRouter.post("/books/addbook", addBook);
+bookRouter.get("/books/getbooks", getAllbooks)
+bookRouter.get("/books/getsinglebook", getSingleBookbyTitle)
+bookRouter.put("/books/putbook", updatebooks)
+bookRouter.delete("/books/deletebook", deletebook)
+bookRouter.delete("/books/deleteAllbooks", deleteAllbooks)
+
+
 
 module.exports = bookRouter;
